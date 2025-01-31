5f9e06bd8089a70e73a5cb3519b10ace1c51d0da
GageLappin119 <gagelappin119@gmail.com>
2025-01-31 17:44:38 -0500

    Update test_assignments_1.py

--- COT-4500-Pro1/src/test/test_assignments_1.py
+++ COT-4500-Pro1/src/test/test_assignments_1.py
@@ -13,13 +13,13 @@ from src.main.assignment_1 import (
 )
 
 class TestAssignment1(unittest.TestCase):
-    
+
     def test_approximation_alg(self):
         approx_alg_bool = True
-        
+
         if not approx_alg_bool:
             return
-            
+
         # Test approximation_alg for various cases
         value_to_approximate = 2
         exponent = 2
@@ -32,7 +32,7 @@ class TestAssignment1(unittest.TestCase):
 
     def test_bisection_method(self):
         bisection_bool = True
-        
+
         if not bisection_bool:
             return
         # Test bisection_method with a simple case
@@ -50,7 +50,7 @@ class TestAssignment1(unittest.TestCase):
     def test_fixed_point_iteration(self):
         fixed_point_bool = True
 
-        
+
         if not fixed_point_bool:
             return
         # Test fixed_point_iteration with a simple function
@@ -59,24 +59,27 @@ class TestAssignment1(unittest.TestCase):
 
         f_0 = 2*x**3 - 2 * x - 5
         f = ((2*x+5) / 2)**(1/3)
-        
+
         error = 0.00001
 
         result = fixed_point_iteration(error, f, f_0)
+        if result == 0:
+            print("Fixed point iteration failed due to divergence.")
+            return
         self.assertGreaterEqual(result, 0, "Iterations should be non-negative.")
         print(f"\nIterations for fixed_point_iteration: {result}")
         print("---------------------------------------")
 
     def test_newton_raphson_method(self):
         newton_raphson_bool = True
-        
+
         if not newton_raphson_bool:
             return
         # Test Newton-Raphson method with a simple function
         x = symbols('x')
         f = x**3 + 4*x**2 - 10
         f_prime = f.diff(x)
-        
+
         initial_guess = 1
         tolerance = 10 ** -4
 
@@ -87,4 +90,3 @@ class TestAssignment1(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.TextTestRunner(verbosity=0).run(unittest.TestLoader().loadTestsFromTestCase(TestAssignment1))
-
